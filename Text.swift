2fc0818ed9c38fc047839c00f3530117ca912ee9
hljames <haileyjames@college.harvard.edu>
2019-02-07 22:20:12 -0500

    first pass

---
+++ Text.swift
@@ -0,0 +1,12 @@
+class Text {
+  content : String
+  date : Int
+  location : (Int,Int)
+  language : String
+  device : String
+  class author {
+    name : String
+    id : Int
+    type : String
+  }
+}
---
+++ TextAnnotation.swift
@@ -0,0 +1,4 @@
+class TextAnnotation : Annotation {
+  // Start and stop char indices in text
+  interval : (Int,Int)
+}
