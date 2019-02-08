ca59d4c2a2ff8f36f8c2c124f2b61c0c309a5804
hljames <haileyjames@college.harvard.edu>
2019-02-08 08:04:23 -0500

    first pass

--- Text.swift
+++ Text.swift
@@ -1,12 +1,20 @@
+//
+//  Text.swift
+//
+//
+//  Created by Hailey James on 2/8/19.
+//
+
+import UIKit
+
+struct Author {
+    var name = ""
+    var id = 0
+    var type = ""
+}
+
 class Text {
-  content : String
-  date : Int
-  location : (Int,Int)
-  language : String
-  device : String
-  class author {
-    name : String
-    id : Int
-    type : String
-  }
+    var author = Author(name: "Hailey", id: 41904090, type = "PERSON")
+    var content = "This sled belong’s to my sister’s friend. We drove past her house on the way up the canyon to the reservoir and got it out of the bed of the truck. Luckily she didn’t mind!"
+    var annotations = []
 }
--- TextAnnotation.swift
+++ TextAnnotation.swift
@@ -1,4 +1,17 @@
-class TextAnnotation : Annotation {
-  // Start and stop char indices in text
-  interval : (Int,Int)
+//
+//  TextAnnotation.swift
+//  
+//
+//  Created by Hailey James on 2/8/19.
+//
+
+import UIKit
+
+class TextAnnotation {
+    var filename = "sister.rtf"
+    var type = "text"
+    var fileformat = "rtf"
+    var category = "PERSON"
+    var interval = (25,30)
 }
+
