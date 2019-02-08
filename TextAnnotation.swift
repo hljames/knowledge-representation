460fd34131f027173a0656d61c82cbef312bfd7b
Hailey James <haileyjames@college.harvard.edu>
2019-02-08 10:27:47 -0500

    Update TextAnnotation.swift

--- TextAnnotation.swift
+++ TextAnnotation.swift
@@ -11,6 +11,7 @@ class TextAnnotation {
     var filename = "sister.rtf"
     var type = "text"
     var fileformat = "rtf"
+    // Category should answer the question: What does this annotation explain? (EX: person, place, event, object, feeling)
     var category = "PERSON"
     var interval = (25,30)
 }
